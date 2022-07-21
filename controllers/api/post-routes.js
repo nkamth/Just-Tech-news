56a20094db38f59e71f6d1d8444583103973ba06
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-07-21 01:06:54 -0400

    defines home routes and homepage handlebar

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,11 +1,32 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const sequelize = require("../../config/connection");
+const { Post, User, Vote, Comment } = require("../../models");
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
     order: [["created_at", "DESC"]],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -24,7 +45,18 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
@@ -58,7 +90,16 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     });
 });
-
+// PUT /api/posts/upvote
+router.put("/upvote", (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 router.put("/:id", (req, res) => {
   Post.update(
     {
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
+const path = require("path");
+const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
 
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
 // turn on routes
 app.use(routes);
 
